4c08f86b87fa0b08b27483ff70995859a0d269c3
miakimamelia <128097163+miakimamelia@users.noreply.github.com>
2023-11-20 12:01:41 -0600

    changes

---
+++ react-three-fiber-shadertoy/src/App.js
@@ -0,0 +1,11 @@
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return (
+    <Canvas style={{ width: "100vw", height: "100vh" }}>
+    
+    </Canvas>
+  );
+}
+
+export default App;
