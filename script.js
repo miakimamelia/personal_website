5b00aeddc38900821934826483ce981968094821
miakimamelia <128097163+miakimamelia@users.noreply.github.com>
2023-12-08 03:49:36 -0600

    changes

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
+// Function to show modal on page load
+window.onload = function () {
+    // Get the modal, iframe, and close button
+    var modal = document.getElementById('myModal');
+    var closeModalBtn = document.getElementById('closeModal');
+    var iframe = document.getElementById('modalIframe');
 
-// "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
-  dialog.showModal();
-});
+    // Show the modal
+    modal.style.display = 'block';
 
-// "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-  dialog.close();
-});
+    // Set iframe source when modal is displayed
+    iframe.src = "/splash/index.html";
+
+    // Close the modal when the close button is clicked
+    closeModalBtn.onclick = function () {
+        modal.style.display = 'none';
+    };
+
+    // Close the modal if the user clicks outside of it
+    document.getElementById('enterButton').onclick = function () {
+        modal.style.display = 'none';
+    };
+};
+
